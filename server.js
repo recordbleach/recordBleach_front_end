7f7dd3e5d9947a081465590e0314df7ee290ec08
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2016-06-20 13:35:25 -0500

    added .gitignore and jquery dependency

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 const theRoot = __dirname + '/dist/',
-	PORT = process.env.PORT || 3000
+	PORT = process.env.PORT || 3000 
 
+// got env port for heroku or elsewhere, else set to 3000 for dev
 app.set('port', PORT)
 
+// get the page
 app.get('/', function (req, res) {
   res.sendFile(theRoot + 'index.html');
 });
 
+// serving static files from dist/
 app.get('/:filename', function (req, res) {
   res.sendFile(theRoot + req.params.filename);
 });
 
+// serving images from dist/images/
 app.get('/images/:filename', function (req, res) {
   res.sendFile(theRoot + 'images/' + req.params.filename);
 });
