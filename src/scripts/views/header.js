d26ff0201b30d9a3e8af9458203bbbad2a48ae62
Caitlin Flattery <caitlinflattery@gmail.com>
2016-11-18 13:06:30 -0600

    added nav bar

--- src/scripts/views/header.js
+++ src/scripts/views/header.js
@@ -5,6 +5,11 @@ var Header = React.createClass({
 		return (
 			<header>
 				<h1>Record Bleach</h1>
+                <nav>
+                    <a href = '#home'>Home</a>
+                    <a href = '#qualifyForm'>Do I Qualify?</a>
+                    <a href = '#login'>Login</a>
+                </nav>
 			</header>
 		)
 	}
